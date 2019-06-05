f236461d123d25134b79d7fd8fc0819409deea34
andrewinsoul <andrewinsoul@gmail.com>
2019-06-05 13:05:30 +0100

    enable jwt authorization

--- myDiary_API/apps/authentication/models.py
+++ myDiary_API/apps/authentication/models.py
@@ -2,11 +2,10 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 import datetime
 
-
 class UserManager(BaseUserManager):
     def create_user(self, username, email, password=None):
         if username is None:
-            raise TypeError('Users nust have a username')
+            raise TypeError('Users must have a username')
         if email is None:
             raise TypeError('Users must have an email address')
         user = self.model(username=username, email=self.normalize_email(email))
--- myDiary_API/apps/authentication/schema.py
+++ myDiary_API/apps/authentication/schema.py
@@ -1,36 +1,40 @@
 from .models import User
-import graphene
+import graphene, django_filters
+from graphene import relay, ObjectType
 from graphene_django import DjangoObjectType
+from graphene_django.filter import DjangoFilterConnectionField
 
-class UserType(DjangoObjectType):
+class UserNode(DjangoObjectType):
     class Meta:
         model = User
+        filter_fields = []
+        interfaces = (relay.Node, )
 
 class UserInput(graphene.InputObjectType):
     id = graphene.ID()
-    username = graphene.String()
-    fname = graphene.String()
-    lname = graphene.String()
-    password = graphene.String()
-    email = graphene.String()
+    username = graphene.String(required=True)
+    fname = graphene.String(required=False)
+    lname = graphene.String(required=False)
+    password = graphene.String(required=True)
+    email = graphene.String(required=True)
 
-class CreateUser(graphene.Mutation):
-    class Arguments:
-        input = UserInput(required=True)
-    user = graphene.Field(UserType)
 
-    def mutate(self, info, **input):
-        input = input['input']
-        user = User(
-            username = input.get('username'),
-            fname = input.get('fname'),
-            lname = input.get('lname'),
-            email = input.get('email')
-        )
-        user.set_password(input.get('password'))
-        user.save()
-        return CreateUser(user=user)
+class CreateUser(relay.ClientIDMutation):
+    class Input:
+        user = graphene.Argument(UserInput)
+    new_user = graphene.Field(UserNode)
+
+    @classmethod
+    def mutate_and_get_payload(cls, root, info, **input):
+        user_data = input.get('user')
+        new_user = User.objects.create(**user_data)
+        new_user.set_password(user_data.get('password'))
+        new_user.save()
+        return cls(new_user=new_user)
 
 class Mutation(graphene.ObjectType):
     create_user = CreateUser.Field()
 
+class Query(graphene.ObjectType):
+    user = relay.Node.Field(UserNode)
+    users = DjangoFilterConnectionField(UserNode)
