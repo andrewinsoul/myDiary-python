992d1b4b7b1d59f2ab068df8d7bff211e1ab8b44
Andrew Okoye <andrewinsoul@gmail.com>
2019-10-28 11:30:47 +0100

    add feature to return token when user signup

--- myDiary_API/apps/authentication/schema.py
+++ myDiary_API/apps/authentication/schema.py
@@ -3,6 +3,8 @@ import graphene, django_filters
 from graphene import relay, ObjectType
 from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
+from os import getenv
+from jwt import encode
 
 class UserNode(DjangoObjectType):
     class Meta:
@@ -23,6 +25,7 @@ class CreateUser(relay.ClientIDMutation):
     class Input:
         user = graphene.Argument(UserInput)
     new_user = graphene.Field(UserNode)
+    token = graphene.String()
 
     @classmethod
     def mutate_and_get_payload(cls, root, info, **input):
@@ -30,7 +33,14 @@ class CreateUser(relay.ClientIDMutation):
         new_user = User.objects.create(**user_data)
         new_user.set_password(user_data.get('password'))
         new_user.save()
-        return cls(new_user=new_user)
+        token = encode(
+            {
+                'email': new_user.email,
+                'password': new_user.password
+            },
+            getenv('SECRET_KEY')
+        ).decode('utf-8')
+        return cls(new_user=new_user, token=token)
 
 class Mutation(graphene.ObjectType):
     create_user = CreateUser.Field()
--- myDiary_API/settings/base.py
+++ myDiary_API/settings/base.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from datetime import timedelta
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -103,4 +104,11 @@ GRAPHENE = {
     'MIDDLEWARE': [
         'graphql_jwt.middleware.JSONWebTokenMiddleware',
     ],
+}
+
+GRAPHQL_JWT = {
+    "JWT_VERIFY_EXPIRATION": True,
+    "JWT_EXPIRATION_DELTA": timedelta(hours=2),
+    "JWT_AUTH_HEADER_PREFIX": "",
+    "JWT_SECRET_KEY": os.getenv("SECRET_KEY")
 }
